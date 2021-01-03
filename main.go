e4b48f226c68ae51496e5b7fedca02d3394a0964
Andjaradji <andjaradji.rooseno@klikacc.com>
2021-01-03 17:27:21 +0700

    get campaignd endpoint (WIP)

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"bwastartup/handler"
 	"bwastartup/helper"
 	"bwastartup/user"
-	"fmt"
 	"net/http"
 	"strings"
 
@@ -37,11 +36,9 @@ func main() {
 	campaignService := campaign.NewService(campaignRepository)
 	authService := auth.NewJwtService()
 
-	campaigns, _ := campaignService.GetCampaigns(9)
-
-	fmt.Println(len(campaigns))
-
 	userHandler := handler.NewUserHandler(userService, authService)
+	campaignHandler := handler.NewCampaignHandler(campaignService)
+
 
 	router := gin.Default()
 	api := router.Group("/api/v1")
@@ -50,6 +47,8 @@ func main() {
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailibility)
 	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
+	api.GET("/campaigns", campaignHandler.GetCampaigns)
+
 
 	router.Run()
 
