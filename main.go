dca2da7ec6af1d1fbe50c98ea2e437313e8c921d
Andjaradji <andjaradji.rooseno@klikacc.com>
2021-01-03 11:10:06 +0700

    add middleware

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"bwastartup/auth"
 	"bwastartup/handler"
+	"bwastartup/helper"
 	"bwastartup/user"
+	"net/http"
+	"strings"
 
 	"log"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 
 	"gorm.io/driver/mysql"
@@ -36,9 +40,58 @@ func main() {
 	api.POST("/users", userHandler.RegisterUser)
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailibility)
-	api.POST("/avatars", userHandler.UploadAvatar)
+	api.POST("/avatars",authMiddleware(authService, userService) ,userHandler.UploadAvatar)
 
 
 	router.Run()
 
 }
+
+func authMiddleware (authService auth.Service, userService user.Service) gin.HandlerFunc {
+	return func  ( c *gin.Context){
+		authHeader := c.GetHeader("Authorization")
+	
+		if !strings.Contains(authHeader, "Bearer"){
+			response := helper.APIResponse("Unauthorized",http.StatusUnauthorized,"No Authorization", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+	
+		tokenString := ""
+		arrayToken := strings.Split(authHeader, " ")
+		if len(arrayToken) == 2 {
+			tokenString = arrayToken[1]
+		}
+
+		token, err := authService.ValidateToken(tokenString)
+
+		if err != nil {
+			response := helper.APIResponse("Unauthorized",http.StatusUnauthorized,"No Valid Token", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		claim , ok := token.Claims.(jwt.MapClaims)
+
+		if !ok || !token.Valid {
+			response := helper.APIResponse("Unauthorized",http.StatusUnauthorized,"Token not found", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		userID := int (claim["user_id"].(float64))
+
+		user, err := userService.GetUserByID(userID)
+
+		if err != nil {
+			response := helper.APIResponse("Unauthorized",http.StatusUnauthorized,"No Authorization", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		c.Set("currentUser", user)
+
+	}
+}
+
+
